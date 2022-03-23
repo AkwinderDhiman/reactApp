31e1b0567e447112ab459871ab221782374d57bb
AkwinderDhiman <akwinder128@gmail.com>
2022-03-23 11:43:18 +0530

    commit

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,11 +3,15 @@ import "./BuildControl.css";
 
 
 const BuildControl =(props)=>{
-   <div className="BuildControl"> 
-       <div className="Label"> {props.label} </div>
-       <button className="Less">Less </button>
-       <button className="More">More</button>
-   </div>
+    return(
+
+        <div className="BuildControl"> 
+        <div className="Label"> {props.label} </div>
+        <button className="Less">Less </button>
+        <button className="More">More</button>
+    </div>
+    
+    )
 }
 
 export default BuildControl;
